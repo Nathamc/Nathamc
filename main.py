1643b9d4390dfc7f07fbfa870d082f003a4df6ec
Natham'C <91547906+Nathamc@users.noreply.github.com>
2021-11-15 18:26:44 +0000

    triangulo

---
+++ main.py
@@ -0,0 +1,9 @@
+a=float(input('Digite um número'))
+b=float(input('Digite um número'))
+c=float(input('Digite um número'))
+if (a+b<c) and (a+c<b) or (b+c<a):
+  print('Não é um triângulo')
+else:
+  print('Sim, é um triângulo')
+
+ 
---
+++ phy.py
@@ -0,0 +1,4 @@
+sal=float(input('Digite o seu salário'))
+if sal<2.500 and sal*0.10:
+  print('O seu salário é {}'.format(sal))
+
